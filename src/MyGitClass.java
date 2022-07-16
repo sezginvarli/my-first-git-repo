c4103066d2984ec81a3de8ac84120531c7da4c4d
Sezgin VARLI <varlisezginn@gmail.com>
2022-07-16 22:47:03 +0300

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -2,6 +2,8 @@ public class MyGitClass {
     public static void main(String[] args) {
 
         System.out.println("I am learning GitHub");
+        System.out.println("I am adding new code...");
+        System.out.println("I am adding one more");
     }
 
 
